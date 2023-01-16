f1ad13aad041aed849cdc36df298416bfcce1d0c
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-01-16 19:22:32 -0300

    Like a photo

--- backend/src/routes/PhotoRoutes.ts
+++ backend/src/routes/PhotoRoutes.ts
@@ -2,10 +2,21 @@ import express from "express";
 const routerPhoto = express.Router();
 
 // Controller
-import { deletePhoto, getAllPhotos, getPhotoById, getUserPhotos, insertPhoto, updatePhoto } from "../controllers/PhotoController";
+import {
+  deletePhoto,
+  getAllPhotos,
+  getPhotoById,
+  getUserPhotos,
+  insertPhoto,
+  likePhoto,
+  updatePhoto,
+} from "../controllers/PhotoController";
 
 // Middlewares
-import { photoInsertValidation, photoUpdateValidation } from "../validations/PhotoValidations";
+import {
+  photoInsertValidation,
+  photoUpdateValidation,
+} from "../validations/PhotoValidations";
 import authGuard from "../middlewares/authGuard";
 import validate from "../middlewares/handleValidation";
 import { imageUpload } from "../middlewares/imageUpload";
@@ -19,10 +30,17 @@ routerPhoto.post(
   validate,
   insertPhoto
 );
-routerPhoto.delete("/:id", authGuard, deletePhoto)
-routerPhoto.get("/", authGuard, getAllPhotos)
-routerPhoto.get("/user/:id", authGuard, getUserPhotos)
-routerPhoto.get("/:id", authGuard, getPhotoById)
-routerPhoto.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto)
+routerPhoto.delete("/:id", authGuard, deletePhoto);
+routerPhoto.get("/", authGuard, getAllPhotos);
+routerPhoto.get("/user/:id", authGuard, getUserPhotos);
+routerPhoto.get("/:id", authGuard, getPhotoById);
+routerPhoto.put(
+  "/:id",
+  authGuard,
+  photoUpdateValidation(),
+  validate,
+  updatePhoto
+);
+routerPhoto.put("/like/:id", authGuard, likePhoto);
 
 export default routerPhoto;
