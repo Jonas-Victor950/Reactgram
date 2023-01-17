261870a83959243e92b09759a10f950ad9a3b67d
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-01-17 20:33:50 -0300

    Setting React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
 
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
 function App() {
   return (
     <div className="App">
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
